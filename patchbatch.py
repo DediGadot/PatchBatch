6b68ffa323e524d8f00cc26c232ad51f372b9e64
Dedi Gadot <dedigadot@gmail.com>
2016-10-15 17:28:48 +0300

    updated API to add --descs option

--- patchbatch.py
+++ patchbatch.py
@@ -1,4 +1,5 @@
 import lasagne
+import sys
 import numpy
 import pb_NN as NN
 import pb_Models as Models
@@ -13,13 +14,8 @@ import os
 import cPickle as pickle
 
 DEBUG = False
-
 patch_size = 51
 batch_size = 256
-# pm params suitable for kitti2012
-pm_params = (2, 5, 10, 500) # num iters, random search iters, max_h, max_w
-pm_iters, pm_rs, rand_ann_h, rand_ann_w = pm_params
-
 
 def calc_descs(img1_filename, img2_filename, model_name):
     """ given two image files and a CNN model name, calculates the dense descriptor tensor of both images
@@ -41,7 +37,7 @@ def calc_descs(img1_filename, img2_filename, model_name):
 
         # reflect borders
         img = cv2.copyMakeBorder(img,patch_size/2,patch_size/2,patch_size/2,patch_size/2,cv2.BORDER_REFLECT_101)
-        print 'image shape after reshape', img.shape
+        print 'image shape after reflect', img.shape
 
         # extract patches and reshape
         patches = image.extract_patches_2d(img, (patch_size, patch_size))
@@ -60,7 +56,7 @@ def calc_patchmatch_and_cost(img1_descs, img2_descs, pm_params, both = False):
     """ Given two descriptor tensors, and PatchMatch params, calculate the ANN
         img1_descs - <h,w,1,#d> dense descriptor tensor for source image
         img2_descs - <hw,w,1,#d> dense descriptor tensor for target image
-        pm_params - <x,y,z,w> (see default values above):
+        pm_params - <x,y,z,w>:
             x - number of PatchMatch iters to do
             y - number of random searches to do, as part of PatchMatch
             z - max h value for initial random ANN
@@ -78,7 +74,7 @@ def calc_patchmatch_and_cost(img1_descs, img2_descs, pm_params, both = False):
     h,w = img1_descs.shape[:2]
 
     print 'calculating patchmatch A->B'
-    rand_ann_h, rand_ann_w = pm_params[2:4]
+    pm_iters, pm_rs, rand_ann_h, rand_ann_w = pm_params
     rand_ann = utils.create_random_ann(h, w, rand_ann_h, rand_ann_w)
     ann_AB, matchcost_AB = patchmodule.patchmatch(img1_descs, img2_descs, numpy.copy(rand_ann),pm_iters = pm_iters, rs_start = pm_rs)
 
@@ -103,10 +99,15 @@ def calc_patchmatch_and_cost(img1_descs, img2_descs, pm_params, both = False):
     return res
 
 
-def calc_flow_and_cost(img1_descs, img2_descs, eliminate_bidi_errors = False):
+def calc_flow_and_cost(img1_descs, img2_descs, pm_params, eliminate_bidi_errors = False):
     """ Given two descriptor tensors, calculate PatchMatch and return flow + cost
         img1_descs - <h,w,1,#d> descriptor tensor for the source image
         img2_descs - <h,w,1,#d> descriptor tensor for the target image
+        pm_params - <x,y,z,w>:
+            x - number of PatchMatch iters to do
+            y - number of random searches to do, as part of PatchMatch
+            z - max h value for initial random ANN
+            w - max w value for initial random ANN
         eliminate_bidi_errors - if True, mark as invalid correspondences which
         do not meet the bidirectional consistency check
 
@@ -140,11 +141,21 @@ if __name__ == '__main__':
     parser.add_argument('model_name', help = 'Name of network to run')
     parser.add_argument('output_path', help = 'Path to where to place the results')
     parser.add_argument('--bidi', help  = 'Run bidirectional consistency test, mark invalid correspondences as such', action='store_true')
+    parser.add_argument('--descs', help  = 'Save descriptors to file', action='store_true')
+    parser.add_argument('--debug', help  = 'Downscales input images by a factor of 4 for debugging purposes', action='store_true')
 
     parser = parser.parse_args()
 
     if not os.path.exists(parser.output_path):
         os.mkdir(parser.output_path)
+
+    if 'KITTI' in parser.model_name:
+        pm_params = (2, 5, 10, 500) # pm_iters, pm_random_search_iters, rand_max_h, rand_max_w
+    elif 'MPI' in parser.model_name:
+        pm_params = (2, 20, 10, 10)
+    else:
+        print 'Error! Unsupported pm_params'
+        sys.exit()
 
 
     #model_name = 'KITTI2012_CENTSD_ACCURATE'
@@ -155,10 +166,14 @@ if __name__ == '__main__':
     #img_descs = calc_descs(img1_filename, img2_filename, model_name)
     #flow_res, cost_res = calc_flow_and_cost(img_descs[0], img_descs[1], True)
 
+    if parser.debug:
+        DEBUG = True
+        print 'DEBUG mode is', DEBUG
+
     print 'Calculating descriptors...'
     img_descs = calc_descs(parser.img1_filename, parser.img2_filename, parser.model_name)
     print 'Calculating flow fields and matching cost'
-    flow_res, cost_res = calc_flow_and_cost(img_descs[0], img_descs[1], parser.bidi)
+    flow_res, cost_res = calc_flow_and_cost(img_descs[0], img_descs[1], pm_params, parser.bidi)
 
     print 'Saving outputs to', parser.output_path
     with open(parser.output_path + '/flow.pickle','wb') as f:
@@ -167,7 +182,8 @@ if __name__ == '__main__':
     with open(parser.output_path + '/cost.pickle','wb') as f:
         pickle.dump(cost_res, f)
 
-    with open(parser.output_path + '/descs.pickle', 'wb') as f:
-        pickle.dump(img_descs, f)
+    if parser.descs:
+        with open(parser.output_path + '/descs.pickle', 'wb') as f:
+            pickle.dump(img_descs, f)
 
-    #kittitool.flow_visualize(flow_res, mode='Y')
+    #kittitool.flow_visualize(flow_res, save_filename=parser.output_path + '/flow.png', mode='Y')
